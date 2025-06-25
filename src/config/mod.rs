cc42b4ef76d0f61e995f4dff9c4c1f20f281e4b4
GM Titou <pochebonnebaptiste@gmail.com>
2025-06-26 00:55:04 +0200

    change deplacement

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -22,35 +22,4 @@ impl Config {
             seed,
         }
     }
-
-    pub fn prompt_with_default<T>(name: &str, default: T) -> T
-    where
-        T: std::str::FromStr + std::fmt::Display,
-        <T as std::str::FromStr>::Err: std::fmt::Debug,
-    {
-        loop {
-            print!("> {} [{}]: ", name, default);
-            io::stdout().flush().unwrap();
-
-            let mut input = String::new();
-            if io::stdin().read_line(&mut input).is_err() {
-                println!("⚠️ Error reading input. Using default.");
-                return default;
-            }
-
-            let trimmed = input.trim();
-            if trimmed.is_empty() {
-                return default;
-            }
-
-            match trimmed.parse() {
-                Ok(val) => return val,
-                Err(_) => {
-                    println!(
-                        "❌ Invalid input. Please enter a valid number or press Enter for default."
-                    );
-                }
-            }
-        }
-    }
 }
